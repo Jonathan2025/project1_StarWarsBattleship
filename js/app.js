7f7458cedd60d526205fa1a1b4547a088ccc85ad
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-01-28 13:01:46 -0500

    attempt 1 at creating a grid structure

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-alert("Js is working")
+// SECTION 1 - Create the Grid 
+const grid = document.getElementById("grid")
+
+for(let i=0; i<6; i++){
+    //add boxes to the grid
+    const box = document.createElement("div")
+    box.classList.add("box")
+    box.innerHTML = i +1
+    grid.appendChild(box)
+}
+
