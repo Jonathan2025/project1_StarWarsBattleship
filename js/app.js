8fe1b80946d6493742f5aef9ef9ee96f722a3eb7
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-01-31 23:20:02 -0500

    change the size of the grid boxes

--- js/app.js
+++ js/app.js
@@ -21,8 +21,6 @@ let cloneArmy = document.querySelector("#cloneCount")
 let droidArmy = document.querySelector("#droidCount")
 let heros = document.querySelector("#heroCount")
 let villans = document.querySelector("#villanCount")
-let clonesDestroyed = document.querySelector("#cloneCasualties")
-let droidsDestroyed = document.querySelector("#droidCasualties")
 let player1Points = document.querySelector("#player1Points")
 let player2Points = document.querySelector("#player2Points")
 const startBtn = document.querySelector("#start")
@@ -98,7 +96,6 @@ class Player1 {
     constructor(){
         this.side = "Clone Army"
         this.cloneArmy = document.querySelector("#cloneCount")
-        this.clonesDestroyed = document.querySelector("#cloneCasualties")
         this.heros = document.querySelector("#heroCount")
         // initialize the points as 0 for now
         this.points = document.querySelector("#player1Points")
@@ -111,10 +108,6 @@ class Player1 {
 
     getArmyCount(){
         return this.cloneArmy.innerHTML
-    }
-
-    getCasualties(){
-        return this.clonesDestroyed.innerHTML
     }
 
     getHeroCount(){
@@ -158,8 +151,11 @@ class Player1 {
         let boxesToHighlight = boxesToHighlightPick
         // for the boxes within the boxesToHighlight, add them to a class where we can add highlight with css later
         for (const boxToHighlight of boxesToHighlight){
+            //make the pointerEvents to "none" so that the player doesnt click on the same box again
+            boxToHighlight.style.pointerEvents ='none'
             if(boxToHighlight){
                 boxToHighlight.classList.add("hit-highlight")
+                
             }
         }
     }
@@ -176,7 +172,7 @@ class Player2 extends Player1 {
         // the properties that we will be getting for player 2 will be slightly different than player 1
         this.side = "Droid Army"
         this.droidArmy = document.querySelector("#droidCount")
-        this.droidsDestroyed = document.querySelector("#droidCasualties")
+        this.points = document.querySelector("#player2Points")
         this.villans = document.querySelector("#villanCount")
         //this.points = document.querySelector("#player2Points")
         
@@ -191,17 +187,13 @@ class Player2 extends Player1 {
         return this.droidArmy.innerHTML
     }
 
-    getCasualties(){
-        return this.droidsDestroyed.innerHTML
+    getPoints(){
+        return this.points.innerHTML
     }
 
     getHeroCount(){
         return this.villans.innerHTML
     }
-
-    // getPoints(){
-    //     return this.points.innerHTML
-    // }
 
 
     //the other methods like targetHover and targetHit will be 
@@ -234,22 +226,22 @@ function changePlayerStats(boxesToHighlightPick){
                 let imageSource = image.getAttribute("src")
                 // if the box has an image of a clone,
                 if ((imageSource.includes('/Images/clone1.png'))|| (imageSource.includes('/Images/clone2.png')) || (imageSource.includes('/Images/clone3.png')) || (imageSource.includes('/Images/clone4.png'))){
-                    // then add 1 to the casualties and subtract 1 from the clone army
-                    clonesDestroyed.innerHTML = parseInt(clonesDestroyed.innerHTML) + 1
+                    // then add 1 to player 2's points and subtract 1 from the clone army (player 1)
+                    player2Points.innerHTML = parseInt(player2Points.innerHTML) + 1
                     cloneArmy.innerHTML = parseInt(cloneArmy.innerHTML) - 1
                 //else if the box has an image of a hero 
                 } else if ((imageSource.includes('/Images/obiwan.png'))|| (imageSource.includes('/Images/yoda.png'))){
-                    // then subtract 1 from heros and add 5 to casualties(clones destroyed) heros are worth more
-                    clonesDestroyed.innerHTML = parseInt(clonesDestroyed.innerHTML) + 5
+                    // then subtract 1 from heros and add 5 points to player 2 side 
+                    player2Points.innerHTML = parseInt(player2Points.innerHTML) + 5
                     heros.innerHTML = parseInt(heros.innerHTML) - 1
                 //else if the box has an image of a droid...
                 } else if ((imageSource.includes('/Images/droid1.png')) || (imageSource.includes('/Images/droid2.png')) || (imageSource.includes('/Images/droid3.png')) || (imageSource.includes('/Images/droid4.png'))){
-                    // then add 1 to the casualties and subtract 1 from the droid army
-                    droidsDestroyed.innerHTML = parseInt(droidsDestroyed.innerHTML) + 1
+                    // then add 1 to player 1's points and subtract 1 from the droid army (player 2 side)
+                    player1Points.innerHTML = parseInt(player1Points.innerHTML) + 1
                     droidArmy.innerHTML = parseInt(droidArmy.innerHTML) - 1
                 } else if ((imageSource.includes('/Images/dooku.png')) || (imageSource.includes('/Images/grevious.png'))){
-                    // then add 5 to the casualties and subtract 1 from villans
-                    droidsDestroyed.innerHTML = parseInt(droidsDestroyed.innerHTML) + 5
+                    // then add 5 player 1's points and subtract 1 from villans
+                    player1Points.innerHTML = parseInt(player1Points.innerHTML) + 5
                     villans.innerHTML = parseInt(villans.innerHTML) - 1
                 }
             }
@@ -270,10 +262,8 @@ function showLocation(boxesToHighlightPick){
                 let childImages = boxHit.querySelectorAll("img")
                 childImages.forEach(image => image.style.display = 'block')
 
-
                 //play lego break sound
-                playSound()
-                
+                playSound()               
             }
         }
     }
@@ -286,6 +276,15 @@ function playSound() {
     const audio = new Audio("Images/lego-breaking.mp3");
     audio.play();
   }
+
+// function play wars music 
+function playSong() {
+    const audio = new Audio("Images/music.mp3");
+    audio.play();
+
+}
+
+
 
 
 //----------------------------------------------------
@@ -302,6 +301,7 @@ startBtn.addEventListener("click", function(){
     addPlayer2PiecestoGrid (boxes,randomPlayer2Indexes, player2pieces)
     // disable the start button once it is clicked on 
     startBtn.disabled = true
+    playSong()
 })
 
 
@@ -315,7 +315,6 @@ function player1Click(event){
     let playerTurn = document.querySelector("#playerTurn")
     playerTurn.innerHTML = "Player one's turn!"
     const boxes = document.getElementsByClassName("box")
-    console.log("player 1's turn!")
     
     for (let i=0; i<boxes.length; i++){
         let boxesToHighlightPick = [boxes[i], boxes[i+1], boxes[i+15], boxes[i+16]]
@@ -362,31 +361,33 @@ function player2Click(event) {
         boxes[i].addEventListener("click", player1Click);
 
         }
-        
+//end of player 2 click
     }
 
+
+//SECTION MAIN GAME FUNCTIONALITY
 // add an event listener for all the boxes on the grid 
 for (let i=0; i<boxes.length; i++){
     let boxesToHighlightPick = [boxes[i], boxes[i+1], boxes[i+15], boxes[i+16]]
     boxes[i].addEventListener("click", function (){
-        showLocation(boxesToHighlightPick)
-    })
-    boxes[i].addEventListener("click",function(){
         changePlayerStats(boxesToHighlightPick)
+        showLocation(boxesToHighlightPick)
+        console.log(player1Instance.getPoints())
+        console.log(player2Instance.getPoints())
+
+
+        //else if statment that decides who wins and who loses 
+        if (player1Instance.getPoints() >= 18 ){
+            console.log("player 1 wins!!!!!")
+            alert("player2 wins")
+        } else if (player2Instance.getPoints() >= 18){
+            alert("player2 wins")
+            console.log("player 2 wins!!!!")
+        }
     })
+
+
 }
 
 
 
-//Start here 
-// Decide who wins the game 
-if (player1Instance.getCasualties() > 5){
-    console.log("player 2 wins!!!!!")
-    alert("player2 wins")
-} else if (player2Instance.getCasualties() > 5){
-    alert("player1 wins")
-    console.log("player 1 wins!!!!")
-}
-
-
-// write a function that keeps calling 
